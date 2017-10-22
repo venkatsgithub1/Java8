7782fab93b95b058a7534bdfbda62f44278f096f
venkatsgithub1 <venkatsgithub@gmail.com>
2017-10-22 19:05:46 +0530

    Create Person.java

---
+++ demo/java/lambdas/Person.java
@@ -0,0 +1,24 @@
+package demo.java.lambdas;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person: Name-> " + this.name + " Age-> " + this.age;
+    }
+}
